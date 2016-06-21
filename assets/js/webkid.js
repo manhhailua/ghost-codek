51bed47230d451602c4bbcac8147afaa3caaddf5
manhpt <manhpt@phi.vn>
2016-06-21 13:44:30 +0700

    cdnjs script

--- assets/js/webkid.js
+++ assets/js/webkid.js
@@ -1,29 +1,31 @@
-$(function() {
+$(function () {
 
   var months = ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC'];
 
   function renderSite(data) {
     data = $.xml2json(data);
     var posts = data.channel.item;
-   
+
     renderLatestArticles(posts);
   }
 
   function renderLatestArticles(posts) {
     var $parent = $('.sidebox.latest-articles .sidebox-content');
-    if(!$parent) {return};
+    if (!$parent) {
+      return
+    }
 
-    for(var i = 0; i < Math.min(posts.length, 5); i++) {
+    for (var i = 0; i < Math.min(posts.length, 5); i++) {
       var p = posts[i];
       var date = new Date(p.pubDate);
       var dateStr = date.getDate() + ' ' + months[date.getMonth()] + ' ' + date.getFullYear();
       var $a = $('<a href="' + p.link + '"><div class="date">' + dateStr + '</div><div>' + p.title + '</div></a>');
-      if(i == 4) {
+      if (i == 4) {
         $a.addClass('last');
       }
       $parent.append($a);
     }
-    
+
     $parent.removeClass('loading');
   }
 
